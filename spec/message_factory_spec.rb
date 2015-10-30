c415d4df2db2f853a8ee45930886a5e2f67f7dcc
Ian Katz <iakatz@paypal.com>
2015-10-30 11:45:01 -0400

    Add ability to override data types

---
+++ spec/message_factory_spec.rb
@@ -0,0 +1,15 @@
+require 'nmea_plus'
+
+RSpec.describe NMEAPlus::MessageFactory, "#create" do
+  describe "testing the message factory" do
+
+    context "by itself" do
+      it "has basic functions" do
+        expect(NMEAPlus::MessageFactory.parent_module).to eq("FIXME_parent_module")
+        expect(NMEAPlus::MessageFactory.message_class_exists?("foobar")).to eq(false)
+        expect(NMEAPlus::MessageFactory.message_class_exists?("NMEAPlus::MessageFactory")).to eq(true)
+      end
+    end
+
+  end
+end
