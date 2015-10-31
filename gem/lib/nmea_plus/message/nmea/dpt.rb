9ddf932af7f07112845bb5014b4d8cc81f38d81c
Ian Katz <ifreecarve@gmail.com>
2015-10-31 18:02:41 -0400

    add DPT messages

---
+++ gem/lib/nmea_plus/message/nmea/dpt.rb
@@ -0,0 +1,12 @@
+require_relative "base_nmea"
+
+module NMEAPlus
+  module Message
+    module NMEA
+      class DPT < NMEAPlus::Message::NMEA::NMEAMessage
+        field_reader :depth_meters, 1, :_float
+        field_reader :offset_distance, 2, :_float
+      end
+    end
+  end
+end
