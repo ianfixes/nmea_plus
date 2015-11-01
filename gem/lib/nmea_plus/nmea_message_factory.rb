d337657c437e086d34e253652b09fd1a4b144f00
Ian Katz <iakatz@paypal.com>
2015-11-01 12:25:09 -0500

    add ITS messages

--- gem/lib/nmea_plus/nmea_message_factory.rb
+++ gem/lib/nmea_plus/nmea_message_factory.rb
@@ -32,6 +32,7 @@ require_relative "message/nmea/hdm"
 require_relative "message/nmea/hdt"
 require_relative "message/nmea/hfb"
 require_relative "message/nmea/hsc"
+require_relative "message/nmea/its"
 require_relative "message/nmea/osd"
 
 =begin boilerplate for message definitions
