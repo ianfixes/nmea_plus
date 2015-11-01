b5c5cbd9b17a7ab97eae9fff370b340a070777ab
Ian Katz <ifreecarve@gmail.com>
2015-11-01 12:59:38 -0500

    add LCD messages

--- gem/lib/nmea_plus/nmea_message_factory.rb
+++ gem/lib/nmea_plus/nmea_message_factory.rb
@@ -33,6 +33,7 @@ require_relative "message/nmea/hdt"
 require_relative "message/nmea/hfb"
 require_relative "message/nmea/hsc"
 require_relative "message/nmea/its"
+require_relative "message/nmea/lcd"
 require_relative "message/nmea/osd"
 
 =begin boilerplate for message definitions
