32f8d0251e39ec8399ce1e35c8313e5728758064
Ian Katz <ifreecarve@gmail.com>
2015-11-01 16:38:39 -0500

    formatter for date/time

--- gem/lib/nmea_plus/message/base.rb
+++ gem/lib/nmea_plus/message/base.rb
@@ -131,6 +131,25 @@ module NMEAPlus
        end
      end
 
+     def _utc_date_time(d_field, t_field)
+          return nil if t_field.nil? or t_field.empty?
+          return nil if d_field.nil? or d_field.empty?
+
+          # get formats and time
+          time_format = /(\d{2})(\d{2})(\d{2}(\.\d+)?)/
+          date_format = /(\d{2})(\d{2})(\d{2})/
+          now = Time.now
+
+          # crunch numbers
+          begin
+            dmy = date_format.match(d_field)
+            hms = time_format.match(t_field)
+            Time.new(2000 + dmy[3].to_i, dmy[2].to_i, dmy[1].to_i, hms[1].to_i, hms[2].to_i, hms[3].to_f)
+          rescue
+            nil
+          end
+     end
+
    end
  end
 end
--- gem/lib/nmea_plus/message/nmea/rmc.rb
+++ gem/lib/nmea_plus/message/nmea/rmc.rb
@@ -6,25 +6,7 @@ module NMEAPlus
       class RMC < NMEAPlus::Message::NMEA::NMEAMessage
 
         def utc_time
-          # get / check fields
-          t_field = @fields[1]
-          d_field = @fields[9]
-          return nil if t_field.nil? or t_field.empty?
-          return nil if d_field.nil? or d_field.empty?
-
-          # get formats and time
-          time_format = /(\d{2})(\d{2})(\d{2}(\.\d+)?)/
-          date_format = /(\d{2})(\d{2})(\d{2})/
-          now = Time.now
-
-          # crunch numbers
-          begin
-            dmy = date_format.match(d_field)
-            hms = time_format.match(t_field)
-            Time.new(2000 + dmy[3].to_i, dmy[2].to_i, dmy[1].to_i, hms[1].to_i, hms[2].to_i, hms[3].to_f)
-          rescue
-            nil
-          end
+          _utc_date_time(@fields[9], @fields[1])
         end
 
         field_reader :active?, 2, :_av_boolean
