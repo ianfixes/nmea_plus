888f1857709f91f3457b14dca624f8d8c70f2aed
Ian Katz <ifreecarve@gmail.com>
2015-10-31 23:01:50 -0400

    add GST messages

---
+++ gem/lib/nmea_plus/message/nmea/gst.rb
@@ -0,0 +1,18 @@
+require_relative "base_nmea"
+
+module NMEAPlus
+  module Message
+    module NMEA
+      class GST < NMEAPlus::Message::NMEA::NMEAMessage
+        field_reader :gga_fix_time, 1, :_utctime_hms
+        field_reader :total_rms_standard_deviation, 2, :_float
+        field_reader :standard_deviation_semimajor_meters, 3, :_float
+        field_reader :standard_deviation_semiminor_meters, 4, :_float
+        field_reader :semimajor_error_ellipse_orientation_degrees, 5, :_float
+        field_reader :standard_deviation_latitude_meters, 6, :_float
+        field_reader :standard_deviation_longitude_meters, 7, :_float
+        field_reader :standard_deviation_altitude_meters, 8 , :_float
+      end
+    end
+  end
+end
