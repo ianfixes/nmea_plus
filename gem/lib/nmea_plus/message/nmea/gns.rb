1ec8c148aaada018c5d11889cd38b5323d7287d5
Ian Katz <ifreecarve@gmail.com>
2015-10-31 22:19:29 -0400

    add GNS messages

---
+++ gem/lib/nmea_plus/message/nmea/gns.rb
@@ -0,0 +1,28 @@
+
+require_relative "base_nmea"
+
+module NMEAPlus
+  module Message
+    module NMEA
+      class GNS < NMEAPlus::Message::NMEA::NMEAMessage
+        field_reader :fix_time, 1, :_utctime_hms
+
+        def latitude
+          _degrees_minutes_to_decimal(@fields[2], @fields[3])
+        end
+
+        def longitude
+          _degrees_minutes_to_decimal(@fields[4], @fields[5])
+        end
+
+        field_reader :mode, 6, :_string
+        field_reader :satellites, 7, :_integer
+        field_reader :hdrop, 8, :_string
+        field_reader :altitude, 9, :_float
+        field_reader :geoidal_separation, 10, :_float
+        field_reader :data_age, 11, :_float
+        field_reader :differential_reference_station_id, 12, :_integer
+      end
+    end
+  end
+end
