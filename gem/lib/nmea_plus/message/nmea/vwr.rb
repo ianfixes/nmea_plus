11d3ca0c57e493ac6b23df43b0d66fc552f131ec
Ian Katz <ifreecarve@gmail.com>
2015-11-01 23:37:42 -0500

    add VWR messages

---
+++ gem/lib/nmea_plus/message/nmea/vwr.rb
@@ -0,0 +1,15 @@
+require_relative "base_nmea"
+
+module NMEAPlus
+  module Message
+    module NMEA
+      class VWR < NMEAPlus::Message::NMEA::NMEAMessage
+        field_reader :wind_direction_degrees, 1, :_float
+        field_reader :wind_direction_bow, 2, :_string
+        field_reader :speed_knots, 3, :_float
+        field_reader :speed_ms, 5, :_float
+        field_reader :speed_kmh, 7, :_float
+      end
+    end
+  end
+end
