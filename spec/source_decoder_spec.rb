da056c004d49b85ece3073eb6ee67c3af97005a0
Ian Katz <iakatz@paypal.com>
2015-11-03 14:52:53 -0500

    basic test for GPS messages

--- spec/source_decoder_spec.rb
+++ spec/source_decoder_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe NMEAPlus::SourceDecoder, "#parse" do
     end
 
     context "when reading from a file source" do
-      it "reads multipart messages" do
+      it "reads multipart AIS messages" do
         samples = File.join(File.dirname(__FILE__), "samples", "aivdm_message_samples.txt")
 
         sd = NMEAPlus::SourceDecoder.new(File.open(samples))
@@ -48,6 +48,31 @@ RSpec.describe NMEAPlus::SourceDecoder, "#parse" do
         expect(multipart_found).to eq(true)
       end
     end
+
+
+    context "when reading from a file source" do
+      it "reads NMEA messages" do
+        samples = File.join(File.dirname(__FILE__), "samples", "gps_message_samples.txt")
+
+        sd = NMEAPlus::SourceDecoder.new(File.open(samples))
+        called_once = false
+        multipart_found = false
+
+        sd.each_complete_message do |message|
+          called_once = true
+          multipart_found = true if 1 < message.total_messages
+          expect(message.is_a? NMEAPlus::Message::NMEA::NMEAMessage).to eq(true)
+          expect(message.is_a? NMEAPlus::Message::Base).to eq(true)
+          expect(message.all_messages_received?).to eq(true)
+          expect(message.all_checksums_ok?).to eq(true)
+          expect(message.ais.message_type.is_a? Integer).to eq(true)
+        end
+
+        expect(called_once).to eq(true)
+        # expect(multipart_found).to eq(true)
+      end
+    end
+
 
 
   end
