5502636f50d33c7f4b50556bb2ea0180ac2858d5
Ian Katz <iakatz@paypal.com>
2015-11-30 22:29:29 -0500

    Add docs for SourceDecoder

--- lib/nmea_plus.rb
+++ lib/nmea_plus.rb
@@ -3,11 +3,21 @@ require 'nmea_plus/version'
 require 'nmea_plus/generated_parser/parser'
 require 'nmea_plus/generated_parser/tokenizer'
 
+# @author Ian Katz
 module NMEAPlus
-  class SourceDecoder
-    attr_accessor :throw_on_parse_fail
-    attr_accessor :throw_on_unrecognized_type  # typically for development
 
+  # The NMEA source decoder wraps an IO object, converting each_line functionality
+  # to {#each_message} or {#each_complete_message}
+  class SourceDecoder
+    # False by default.
+    # @return [bool] whether to throw an exception on lines that don't properly parse
+    attr_accessor :throw_on_parse_fail
+
+    # False by default. Typically for development.
+    # @return [bool] whether to throw an exception on message types that aren't supported
+    attr_accessor :throw_on_unrecognized_type
+
+    # @param line_reader [IO] The source stream for messages
     def initialize(line_reader)
       unless line_reader.respond_to? :each_line
         fail ArgumentError, "line_reader must inherit from type IO (or implement each_line)"
@@ -17,7 +27,9 @@ module NMEAPlus
       @decoder = NMEAPlus::Decoder.new
     end
 
-    # return each parsed message
+    # Executes the block for every valid NMEA message in the source stream
+    # @yield [NMEAPlus::Message] A parsed message
+    # @return [void]
     def each_message
       @source.each_line do |line|
         if @throw_on_parse_fail
@@ -34,7 +46,10 @@ module NMEAPlus
       end
     end
 
-    # return messages grouped into multipart chains as required
+    # Executes the block for every valid NMEA message in the source stream, attempting to
+    # group multipart messages into message chains.
+    # @yield [NMEAPlus::Message] A parsed message that may contain subsequent parts
+    # @return [void]
     def each_complete_message
       partials = {}
       each_message do |msg|
