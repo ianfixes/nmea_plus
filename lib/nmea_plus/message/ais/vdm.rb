4e940186f9bd7b5bde153d5e78f357a47b2a074e
Ian Katz <ifreecarve@gmail.com>
2015-12-01 22:11:34 -0500

    add AIS message types 18 and 19

--- lib/nmea_plus/message/ais/vdm.rb
+++ lib/nmea_plus/message/ais/vdm.rb
@@ -17,13 +17,14 @@ end
 
 =end
 
-require_relative "vdm_payload/vdm_msg1"
+require_relative "vdm_payload/vdm_msg1" # also incldues 2 and 3
 require_relative "vdm_payload/vdm_msg4"
 require_relative "vdm_payload/vdm_msg5"
 require_relative "vdm_payload/vdm_msg8"
 require_relative "vdm_payload/vdm_msg9"
 require_relative "vdm_payload/vdm_msg12"
 require_relative "vdm_payload/vdm_msg14"
+require_relative "vdm_payload/vdm_msg18" # also includes 19
 
 module NMEAPlus
   module Message
