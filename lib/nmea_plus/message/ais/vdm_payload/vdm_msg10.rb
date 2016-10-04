8cddb67513a8b6c31576b255b10f303be1ad1697
Ian Katz <ifreecarve@gmail.com>
2016-10-04 15:57:23 -0400

    Add support for AIS message type 10

---
+++ lib/nmea_plus/message/ais/vdm_payload/vdm_msg10.rb
@@ -0,0 +1,15 @@
+require_relative 'vdm_msg'
+
+module NMEAPlus
+  module Message
+    module AIS
+      module VDMPayload
+        # AIS Type 10: UTC/Date Inquiry
+        class VDMMsg10 < NMEAPlus::Message::AIS::VDMPayload::VDMMsg
+
+          payload_reader :destination_mmsi, 40, 30, :_u
+        end
+      end
+    end
+  end
+end
